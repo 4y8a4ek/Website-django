38ec8d112bb316d8b6dfe3ed9d90416f3637a215
4y8a4ek <68740549+4y8a4ek@users.noreply.github.com>
2026-03-02 19:54:07 +0700

    feat: add somes math

--- website/website/settings.py
+++ website/website/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d*5a^zw*z+q-f7n2xz4r8pjw4yq9&1abcj+2kve&636bc6=#gt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
